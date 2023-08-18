b4541bd7ba07dfe89118914585d9b74e1c0e7d7e
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-19 00:27:37 +0100

    customize the admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'get_category',  # Use a method to display the category name
+        'price',
+        'rating',
+        'image'
+    )
+
+    ordering = ('sku',)
+
+    def get_category(self, obj):
+        return obj.category.name if obj.category else None
+    get_category.short_description = 'Category'
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )   
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
