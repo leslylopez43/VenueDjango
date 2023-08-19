fc7a7c095f1680c2a6d3e3916ff76b507b01ffcc
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-19 03:52:06 +0100

    butthon
    my ad booking button works

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from .  import views
 
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<venue_id', views.venue_detail, name='venue_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
@@ -10,3 +10,13 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def venue_detail(request, venue_id):
+    """ A view to show all venues, including venue detais"""
+    products = get_object_or_404 (Product, pk=venue_id)  # Use 'products' with lowercase 'p'
+
+    context = {
+        'products': products,  # Use 'products' here as well
+    }
+
+    return render(request, 'products/venue_detail', context)
