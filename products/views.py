6ea989d6c5fb83ef8714ec39bce0bee7cc0d2579
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-21 00:15:00 +0100

    manage to add data

--- products/views.py
+++ products/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render, redirect, reverse,  get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category,Hall, Apartment, Tenant, Tenancy, Accommodation, Payment, BusinessOwner
-
+from .models import Product, Category
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries"""
+    """A view to show all products, including sorting and search queries"""
 
     products = Product.objects.all()
     query = None
-    categories = None  # Initialize categories to None
-
+    categories = None
     if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -28,17 +26,17 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories if categories is not None else [],  # Provide an empty list if categories is None
+        'current_categories': categories 
     }
 
     return render(request, 'products/products.html', context)
 
 def venue_detail(request, venue_id):
-    """ A view to show all venues, including venue detais"""
-    products = get_object_or_404 (Product, pk=venue_id)  # Use 'products' with lowercase 'p'
+    """A view to show venue details"""
+    product = get_object_or_404(Product, pk=venue_id)  # Rename 'products' to 'product'
 
     context = {
-        'products': products,  # Use 'products' here as well
+        'product': product,  # Rename 'products' to 'product' here as well
     }
 
     return render(request, 'products/venue_detail.html', context)
