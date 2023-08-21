dcbb979959af13a6df019f24d197337722618b0b
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-21 20:47:07 +0100

    add
    addd funtionality to add bag

--- bag/urls.py
+++ bag/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .  import views
-
+from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag')  # 
+    path('', views.view_bag, name='view_bag'),  # Add a comma here
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),  # Corrected the period to a comma
 ]
 
--- bag/views.py
+++ bag/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def view_bag(request):
+    """A view that renders the bag content page"""
+    return render(request, 'bag/bag.html')
 
-def view_bag (request):
-
-    """ A view that renders the bag content name"""
-
-    return render (request, 'bag/bag.html')
-
+def add_to_bag(request, item_id):
+    """Add a quantity of the specific product to the shopping bag"""
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
     
+    bag = request.session.get('bag', {})  # Corrected the typo here
+    
+    if item_id in bag:
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    
+    return redirect(redirect_url)
