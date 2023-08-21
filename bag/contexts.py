2034a5988afe55ae4353bf5249f9e1a1342b21a2
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-21 21:21:23 +0100

    update context
    procesor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,15 +1,23 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
-    # Calculate total and product_count based on bag contents
-    for item in bag_items:
-        total += item.price * item.quantity
-        product_count += item.quantity
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            "item_id": item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_MEMBERS_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
