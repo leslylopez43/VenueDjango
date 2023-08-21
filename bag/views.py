577aaa32631ac95ca20ded6993e6580a500840c6
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-21 22:28:38 +0100

    finishing product add
    logic

--- bag/views.py
+++ bag/views.py
@@ -8,15 +8,24 @@ def add_to_bag(request, item_id):
     """Add a quantity of the specific product to the shopping bag"""
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    
-    bag = request.session.get('bag', {})  # Corrected the typo here
-    
-    if item_id in bag:
-        bag[item_id] += quantity
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']  # Fixed the variable name here
+    bag = request.session.get('bag', {})
+
+    if size:
+        if item_id in bag.keys():  # Corrected the typo "bag.key()" to "bag.keys()"
+            if size in bag[item_id]['items_by_size'].keys():  # Corrected "esle" to "else"
+                bag[item_id]['items_by_size'][size] += quantity  # Fixed capitalization of "Quantity"
+            else: 
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in bag:
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
-    
-    
     return redirect(redirect_url)
