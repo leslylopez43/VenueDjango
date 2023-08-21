782a206197fa6c468c5fe551602353534c0fd4aa
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-21 02:41:54 +0100

    update a new app

--- bag/views.py
+++ bag/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 
 
 
-def view_bag (request):
+def view_bag(request):
     """ A view that render the bag contents page"""
-    
+
     return render (request, 'bag/bag.html')
 
     
