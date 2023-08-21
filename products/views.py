838c7538a0bbb6003782a9efafc3b42a1bd35f56
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-21 01:51:49 +0100

    added javascript to manage from the botton to the top

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
+
 
 def all_products(request):
     """A view to show all products, including sorting and search queries"""
