83d78f7f1767f900e0589670897aaa08f8daf5cc
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-23 21:38:50 +0100

    products url

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .  import views
-
+from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<venue_id>', views.venue_detail, name='venue_detail'),
+    path('<int:venue_id>/', views.venue_detail, name='venue_detail'),  # Make sure to use 'int:' to capture an integer value
+    path('<int:product_id>/edit/', views.edit_product, name='edit_product'),  # Add this line for the edit URL
 ]
