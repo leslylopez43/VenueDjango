ec577ef09a4486389f03b18a29b4f7749ddb839c
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-23 18:57:29 +0100

    update bag tools bag

--- bag/templatetag/bag_tools.py
+++ bag/templatetag/bag_tools.py
@@ -1,17 +1,7 @@
 from django import template
 
-register = template.library()
+register = template.Library()
 
 @register.filter(name='calc_subtotal')
-def calc_subtotal (price, quantity):
+def calc_subtotal(price, quantity):
     return price * quantity
-
-# @register.filter
-# def custom_filter(value):
-#     # Your filter logic here
-#     return value
-
-# @register.tag
-# def custom_tag(parser, token):
-#     # Your tag logic here
-#     return CustomNode()
