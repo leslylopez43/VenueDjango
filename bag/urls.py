991cc148a94b9396fc8be317214295eef0e7b09e
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-09-02 00:22:53 +0100

    add
    add to the cart

--- bag/urls.py
+++ bag/urls.py
@@ -1,11 +1,18 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.urls import path
+# from . import views
+
+
+
+# urlpatterns = [
+#     path('', views.view_bag, name='view_bag'),
+#     path('adjust_bag/<item_id>/', views.adjust_bag, name='adjust_bag'),  # Update this line
+# ]
+
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    path('adjust_bag/<item_id>/', views.adjust_bag, name='adjust_bag'),  # Update this line
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
-
