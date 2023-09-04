e4a0ceba69fc31497f4db7672fee85814d50dad2
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-09-04 21:11:20 +0100

    update

--- venue_ariel/middleware.py
+++ venue_ariel/middleware.py
@@ -1,13 +1,13 @@
-from django.http import HttpResponseForbidden
+# from django.http import HttpResponseForbidden
 
-class AdminAccessMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+# class AdminAccessMiddleware:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
 
-    def __call__(self, request):
-        # Check user's permissions or roles here
-        if not request.user.is_superuser:
-            return HttpResponseForbidden("You are not authorized to access this page.")
+#     def __call__(self, request):
+#         # Check user's permissions or roles here
+#         if not request.user.is_superuser:
+#             return HttpResponseForbidden("You are not authorized to access this page.")
         
-        response = self.get_response(request)
-        return response
+#         response = self.get_response(request)
+#         return response
