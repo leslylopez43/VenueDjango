23ddf32f940e44127aed384c6e55f89f41806323
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-09-04 21:10:25 +0100

    add

--- home/views.py
+++ home/views.py
@@ -10,18 +10,18 @@ def index (request):
 
 
 
-@login_required
-def admin_dashboard(request):
-# Check if the user is an admin (superuser)
-    if not request.user.is_superuser:
-        return HttpResponseForbidden("You are not authorized to access this page.")
+# @login_required
+# def admin_dashboard(request):
+# # Check if the user is an admin (superuser)
+#     if not request.user.is_superuser:
+#         return HttpResponseForbidden("You are not authorized to access this page.")
 
-    # Your admin-only dashboard logic here
-    # This is where you can retrieve data, perform operations, and render the dashboard template
+#     # Your admin-only dashboard logic here
+#     # This is where you can retrieve data, perform operations, and render the dashboard template
 
-    # Example: Get a list of all users (for demonstration purposes)
-    all_users = User.objects.all()
+#     # Example: Get a list of all users (for demonstration purposes)
+#     all_users = User.objects.all()
 
-    # Example: Render the admin dashboard template with user data
-    return render(request, 'admin_dashboard.html', {'users': all_users})
+#     # Example: Render the admin dashboard template with user data
+#     return render(request, 'admin_dashboard.html', {'users': all_users})
  
