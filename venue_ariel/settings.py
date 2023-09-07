91a747030151824882e554fd390b740c681f13f8
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-09-07 01:58:36 +0100

    update profile

--- venue_ariel/settings.py
+++ venue_ariel/settings.py
@@ -116,6 +116,14 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_app_password'
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
