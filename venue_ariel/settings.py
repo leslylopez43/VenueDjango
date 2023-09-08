881107a9d494a062ffaab143bb103de4062e01e3
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-09-08 21:56:01 +0100

    Confirmation email body
    update webhoo handler to handle profiles

--- venue_ariel/settings.py
+++ venue_ariel/settings.py
@@ -120,8 +120,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_app_password'
+EMAIL_HOST_USER = 'leslylopez43@someemail.com	'
+EMAIL_HOST_PASSWORD = 'London2320'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
