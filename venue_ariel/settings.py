4d5d4beacbad4b23bd59ebb1687a4b6dd782e117
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-09-14 21:09:58 +0100

    update

--- venue_ariel/settings.py
+++ venue_ariel/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)9nf8j^&e=^!z^l=_os1r%3omcq=9c3^1=uwww7oet6!l$)1q!'
-# SECRET_KEY = os.enviorement.get('SECRET_KEY', '')
+SECRET_KEY = os.enviorement.get('SECRET_KEY', '')
 
 
 
