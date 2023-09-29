b9e6c0aa24262446812b188a0956068acde8f081
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-09-29 21:06:31 +0100

    change

--- venue_ariel/settings.py
+++ venue_ariel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-)9nf8j^&e=^!z^l=_os1r
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'False' in os.environ
 
 import os
 import dj_database_url
